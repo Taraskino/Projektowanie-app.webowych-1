0d0e53813dcd374a66186a7baf475d33a8baaa89
muzychka <TarasMuzychka1989@gmail.com>
2022-05-14 16:08:04 +0200

    Strona_zaawiania_piccy

--- src/main/java/pl/wszib/pizzamarket/web/controllers/MenuController.java
+++ src/main/java/pl/wszib/pizzamarket/web/controllers/MenuController.java
@@ -4,11 +4,16 @@ import org.hibernate.boot.archive.scan.internal.PackageDescriptorImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.wszib.pizzamarket.data.entities.PizzaEntity;
 import pl.wszib.pizzamarket.data.repositores.PizzaRepository;
+import pl.wszib.pizzamarket.web.models.OrderAddressModel;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("menu")
@@ -24,5 +29,15 @@ public class MenuController {
         List<PizzaEntity> pizzas = pizzaRepository.findAll();
         model.addAttribute("pizzas", pizzas);
         return "menuPage";
+    }
+
+    @GetMapping("order/{pizzaId}")
+    public String showPizzaOrderPage(
+            @PathVariable Long pizzaId,
+            @ModelAttribute ("orderAddress") OrderAddressModel orderAddressModel,
+            Model model){
+        PizzaEntity pizza =  pizzaRepository.findById(pizzaId).orElseThrow(EntityNotFoundException::new);
+        model.addAttribute("pizza", pizza);
+        return "orderPizzaPage";
     }
 }
